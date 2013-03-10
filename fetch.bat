c6893bb7ee9f93595c8bc2163e03c1384ff8e7a0
Erez Shin <erezshin+git@gmail.com>
2013-03-10 14:55:03 +0200

    Added vim-commentary to requirements

--- fetch.bat
+++ fetch.bat
@@ -2,3 +2,5 @@
 cd ..
 if not exist python-mode git clone git://github.com/klen/python-mode.git 
 if not exist vim-surround git clone git://github.com/tpope/vim-surround.git
+if not exist vim-commentary git clone git://github.com/tpope/vim-commentary.git
+
