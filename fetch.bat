8b2f1ef5efdd6769e9babc7dac2d71a99c4ca563
Erez Shin <erezshin@gmail.com>
2013-03-14 00:47:09 +0200

    Added repositories to fetch

--- fetch.bat
+++ fetch.bat
@@ -3,4 +3,6 @@ cd ..
 if not exist python-mode git clone git://github.com/klen/python-mode.git 
 if not exist vim-surround git clone git://github.com/tpope/vim-surround.git
 if not exist vim-commentary git clone git://github.com/tpope/vim-commentary.git
-
+if not exist supertab git clone git://github.com/ervandew/supertab.git
+if not exist nerdtree git clone git://github.com/scrooloose/nerdtree.git
+if not exist jedi-vim git clone git://github.com/davidhalter/jedi-vim.git
